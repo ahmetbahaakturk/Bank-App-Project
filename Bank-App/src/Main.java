fe5a5b1bd7d16caeb4b84cea7904ecb2908dcb8d
Ahmet Baha Aktürk <116023192+ahmetbahaakturk@users.noreply.github.com>
2024-05-01 19:38:51 +0300

    Update Main.java

--- Bank-App/src/Main.java
+++ Bank-App/src/Main.java
@@ -4,7 +4,6 @@ import User.User;
 
 public class Main {
     public static void main(String[] args) {
-        new OperationPage(new User(1,"Efe Eren","Kesler","22567988232"));
-        //Operation.run();
+        Operation.run();
     }
 }
